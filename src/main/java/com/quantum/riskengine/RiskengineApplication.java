4999561cee5d1a1a6a63c3746b203fcbd9979a6c
huajun <huajun@zhoudongdeMacBook-Air.local>
2018-12-24 21:03:25 +0800

    Initial commit

---
+++ src/main/java/com/quantum/riskengine/RiskengineApplication.java
@@ -0,0 +1,14 @@
+package com.quantum.riskengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskengineApplication.class, args);
+    }
+
+}
+
